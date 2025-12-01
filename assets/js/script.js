c96a6f0ef9839f95123dfc54d5580fc16fcff370
Erynill <242376261+Erynill@users.noreply.github.com>
2025-12-01 14:58:52 +0100

    afficher film dans tableau + bouton ajouter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,42 @@
 "use strict";
 
+import $ from "jquery";
 import { films } from "./app";
 
 /* ---------------------------------------------------- Variables --------------------------------------------------- */
-
+let tabFilm = films;
 /* ---------------------------------------------------- Functions --------------------------------------------------- */
 
+//Affiche les films dans le tableau
+function displayFilm() {
+    let elemDisplay = tabFilm
+        .map(
+            (elem) => `<tr>
+                        <td class="shell">${elem.title}</td>
+                        <td class="shell">${elem.years}</td>
+                        <td class="shellEnd">${elem.authors}</td>
+                        <td class="shellEnd text-end"><button type="button">Suppr</button></td>
+                    </tr>`
+        )
+        .join("");
+
+    $("#displayFilm").html(elemDisplay);
+}
+
+//Ajoute les listeners sur les boutons pour rajouter des films dans la liste
+function addFilm() {
+    $("#buttonAddFilm").on("click", function () {
+        $(this).addClass("hidden");
+        $("#inputAddFilm").removeClass("hidden").addClass("flex");
+        $("#buttonSaveFilm").on("click", function () {
+            $("#inputAddFilm").removeClass("flex").addClass("hidden");
+            $("#buttonAddFilm").removeClass("hidden");
+        });
+    });
+}
+
+function checkAddFilm() {}
 /* ----------------------------------------------------- Script ----------------------------------------------------- */
+
+displayFilm();
+addFilm();
